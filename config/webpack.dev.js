ca746110bec64159ed5c7daf9c10d8832e052da6
stathis212 <stsaras@zulutrade.com>
2017-11-23 19:02:01 +0200

    First Commit

---
+++ config/webpack.dev.js
@@ -0,0 +1,64 @@
+const commonConfig = require('./webpack.common.js');
+const proxy = require('http-proxy-middleware');
+const helpers = require('./helpers');
+const historyApiFallback = require('connect-history-api-fallback');
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = webpackMerge(commonConfig, {
+    devtool: 'inline-source-map',
+
+    output: {
+        path: helpers.root('dist'),
+        publicPath: '/',
+        filename: 'assets/scripts/[name].bundle.js',
+        chunkFilename: 'assets/scripts/[id].chunk.js'
+    },
+
+    plugins: [
+         new webpack.LoaderOptionsPlugin({
+            options: {
+                minimize: true,
+                tslint: {
+                    emitErrors: true,
+                    failOnHint: true,
+                },
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                API_URL: JSON.stringify('http://localhost:5000/api/')
+            }
+        }),
+        new webpack.NamedModulesPlugin(),
+        new BrowserSyncPlugin({
+        host: 'localhost',
+        port: '3000',
+        server: {
+            baseDir: [helpers.root('dist')],
+            middleware: [
+            proxy('/webservices', {
+                target: process.env.API_URL || 'http://my-test-api.net/',
+                secure: false,
+                changeOrigin: true,
+                ws: true,
+                pathRewrite: {
+                '^/webservices/' : '/',
+                },
+            }),
+            historyApiFallback()
+            ],
+        },
+        open: false,
+        })
+    ],
+
+    devServer: {
+        historyApiFallback: true,
+        stats: 'minimal'
+    },
+});
